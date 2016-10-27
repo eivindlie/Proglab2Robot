153a54d3f70cf3a5640086f8ef503654b0c9fb86
Eivind Lie Andreassen <ela4991@gmail.com>
2016-10-27 11:09:21 +0200

    .

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import robodemo
 from motors import Motors
 
 def main():
+    m = Motors()
     while True:
-        robodemo.random_step(Motors())
+        robodemo.random_step(m)
 
 if __name__ == "__main__":
     main()
