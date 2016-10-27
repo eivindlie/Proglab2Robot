3f6628db32a576aa7096cc32e4f035181cca83c0
Eivind Lie Andreassen <ela4991@gmail.com>
2016-10-27 11:41:54 +0200

    Added BBCON

---
+++ bbcon.py
@@ -0,0 +1,27 @@
+
+
+class BBCON:
+    behaviors = []
+    active_behaviors = []
+    sensobs = []
+    motobs = []
+    arbitrator = None
+
+    def add_behavior(self, behavior):
+        if behavior not in self.behaviors:
+            self.behaviors.append(behavior)
+
+    def add_sensob(self, sensob):
+        if sensob not in self.sensobs:
+            self.sensobs.append(sensob)
+
+    def activate_behavior(self, behavior):
+        if behavior not in self.active_behaviors:
+            self.active_behaviors.append(behavior)
+
+    def deactivate_behavior(self, behavior):
+        if behavior in self.active_behaviors:
+            self.active_behaviors.remove(behavior)
+
+    def run_one_timestep(self):
+        pass
