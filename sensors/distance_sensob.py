e21245b00bf8b2def62c0d7dd07debddc7435470
Eivind Lie Andreassen <ela4991@gmail.com>
2016-10-31 11:18:53 +0100

    .

--- sensors/distance_sensob.py
+++ sensors/distance_sensob.py
@@ -5,5 +5,6 @@ from sensors.sensob import Sensob
 class DistanceSensob(Sensob):
 
     def update(self):
+        self.sensors[0].update()
         self.value = self.sensors[0].get_value()
         print(self.value)
