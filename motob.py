0bbedebec57ec780b2310da1040259b11576425f
Eivind Lie Andreassen <ela4991@gmail.com>
2016-10-31 10:12:36 +0100

    Added basic commands to motob

--- motob.py
+++ motob.py
@@ -1,17 +1,32 @@
+from enum import Enum
+
 class Motob:
 
     def __init__(self, motors):
         self.motors = motors
         self.value = None
 
-    def update(self, motor_recomendation):
-        self.value = motor_recomendation
+    def update(self, motor_recommendation):
+        self.value = motor_recommendation
         self.operationalize()
 
     def operationalize(self):
         # convert motor recomendation into motor settings, and send settings to motors
-        pass
+        c = self.value[0]
+        if c == Command.F: # Forwards
+            self.motors.forward(self.value[1]) # Second parameter in the command is speed
+        elif c == Command.B: # Backwards
+            self.motors.backward(self.value[1]) # Second parameter in the command is speed
+        elif c == Command.L: # Turn left
+            pass
+        elif c == Command.R: # Turn right
+            pass
 
     def stop(self):
-        # Stop all motors
-        pass
+        self.motors.stop()
+
+class Command(Enum):
+    F = 0 # Forward
+    B = 1 # Backward
+    L = 2 # Turn left
+    R = 3 # Turn right
