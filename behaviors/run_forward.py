344da2cf8eb5133235cd84fd0ec2ba99a763aab5
Eivind Lie Andreassen <ela4991@gmail.com>
2016-10-31 11:24:35 +0100

    Changed run_forward to run n seconds from first update

--- behaviors/run_forward.py
+++ behaviors/run_forward.py
@@ -6,9 +6,13 @@ class RunForward(Behavior):
 
     def __init__(self, bbcon, priority, sensobs):
         super().__init__(bbcon, priority, sensobs)
-        self.start_time = time.time()
+        self.start_time = -1
 
     def sense_and_act(self):
+        if self.start_time == -1:
+            self.start_time = time.time()
+            return
+        
         if time.time() - self.start_time < 1:
             self.motor_recommendations = [(Command.F, 0.1)]
         else:
