36d1e0f376297224c96e9487a1f1984397c8ec98
Ole Alexander Hole <olealx@blan.no>
2016-11-07 10:59:03 +0100

    bugfix

--- behaviors/avoid_walls.py
+++ behaviors/avoid_walls.py
@@ -4,6 +4,10 @@ from motob import Command
 # Requires 1 distnace sensob as index 0 and 1 proximity sensob as index 1
 class AvoidWalls(Behavior):
 
+    def __init__(self, bbcon, priority, sensobs):
+        super().__init__(bbcon, priority, sensobs)
+        self.motor_recommendations.append((Command.F, 0))
+
     def sense_and_act(self):
         distance = self.sensobs[0].get_value()
         side_proximity = self.sensobs[1].get_value()
@@ -18,10 +22,10 @@ class AvoidWalls(Behavior):
         front = distance < distance_threshold
 
         if front and not left_side and not right_side:
-            self.motor_recommendations.append((Command.B, 0.5))
+            self.motor_recommendations[0] = (Command.B, 0.5)
             self.match_degree = 1
         else:
+            self.motor_recommendations((Command.F, 0))
             self.match_degree = 0
-
 
 
