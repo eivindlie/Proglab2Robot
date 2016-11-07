90b6998ce4ec96eef2f8310c11d123cf87f383be
Ole Alexander Hole <olealx@blan.no>
2016-11-07 10:28:48 +0100

    Testing avoid walls

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import wiringpi2 as wp
 from bbcon import BBCON
 from behaviors.run_forward import RunForward
+from behaviors.avoid_walls import AvoidWalls
 
 def main():
     wp.wiringPiSetupGpio()
     bbcon = BBCON()
+
     run_forward = RunForward(bbcon, 1, [])
-    bbcon.add_behavior(run_forward)
-    bbcon.activate_behavior(run_forward)
+    #bbcon.add_behavior(run_forward)
+    #bbcon.activate_behavior(run_forward)
+
+    avoid_walls = AvoidWalls(bbcon, 1, [bbcon.sensobs['distance'], bbcon.sensobs['proximity']])
+    bbcon.add_behavior(avoid_walls)
+    bbcon.activate_behavior(avoid_walls)
+
     while True:
         bbcon.run_one_timestep()
 
