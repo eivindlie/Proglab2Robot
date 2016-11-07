93ac8bbdd45b6b64dcb2bc374ca75cb762bc67d0
Eivind Lie Andreassen <ela4991@gmail.com>
2016-11-07 09:53:03 +0100

    Added wall_detected to distance sensob

--- sensors/distance_sensob.py
+++ sensors/distance_sensob.py
@@ -4,6 +4,12 @@ from sensors.sensob import Sensob
 # Initialized with a single ultrasonic sensor
 class DistanceSensob(Sensob):
 
+    def __init__(self, threshold = 15):
+        self.threshold = threshold
+
     def update(self):
         self.value = self.sensors[0].get_value()
         print("distance", self.value)
+
+    def wall_detected(self):
+        return self.value < self.threshold
