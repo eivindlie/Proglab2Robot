ff49d4e875ccad33c4594525ed1993b065403f8b
Your Name <you@example.com>
2016-11-11 11:59:13 +0100

    mot

--- motors.py
+++ motors.py
@@ -9,7 +9,7 @@ class Motors():
         self.setup()
 
     def setup(self):
-        self.max = 700
+        self.max = 600
         self.high = 500
         self.normal = 300
         self.low = 100
