037d2c815c6628db2cb5b9f979d5519b1a70e681
Your Name <you@example.com>
2016-11-11 09:49:56 +0100

    .

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ from sensors.line_pos_sensob import LinePosSensob
 from sensors.proximity_sensob import ProximitySensob
 from sensors.red_search_sensob import RedSearchSensob
 
-from motob import Motob
+from motob import Motob, Command
 from motors import Motors
 
 from sensors.ultrasonic import Ultrasonic
@@ -20,10 +20,11 @@ def main():
 
     motors = Motors()
     motors.stop()
+    motob = Motob(motors)
 
     sensors = {
         #'ultrasonic': Ultrasonic(0.05),
-        'IR': IRProximitySensor(),
+        #'IR': IRProximitySensor(),
         #'reflectance': ReflectanceSensors(False, 0, 900),
         #'camera': Camera(),
     }
@@ -33,10 +34,12 @@ def main():
     sensobs = {
         #'distance': DistanceSensob([sensors['ultrasonic']]),
         #'line_pos': LinePosSensob([sensors['reflectance']]),
-        'proximity': ProximitySensob([sensors['IR']]),
+        #'proximity': ProximitySensob([sensors['IR']]),
         #'red_search': RedSearchSensob([sensors['camera']]),
     }
 
+    commands = [(Command.F, 0.5), (Command.F, 1)]
+    i=0
     while True:
         for sensor in sensors.values():
             sensor.update()
@@ -44,7 +47,10 @@ def main():
         for sensob in sensobs.values():
             sensob.update()
 
+        motob.update(commands[i%len(commands)])
+
         time.sleep(1)
+        i+=1
 
 if __name__ == "__main__":
     main()
